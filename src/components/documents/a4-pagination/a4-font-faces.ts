b507c5453e84deffcea5c0910d914a77b40a490c
Scofieldkoh <109188864+Scofieldkoh@users.noreply.github.com>
2026-09-13 12:10:37 +0800

    S3: keep font-family quoting ES2017-safe

--- src/components/documents/a4-pagination/a4-font-faces.ts
+++ src/components/documents/a4-pagination/a4-font-faces.ts
@@ -75,7 +75,7 @@ function primaryFontFamily(fontFamily: string): string {
 }
 
 function quoteFontFamily(fontFamily: string): string {
-  return `"${fontFamily.replace(/"/g, '\\"')}"`;
+  return `"${fontFamily.split('"').join('\\"')}"`;
 }
 
 /**
